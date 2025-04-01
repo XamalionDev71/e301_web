ea6013626befa8f316822a24a97fab0373750b3c
XamalionDev <132012434+XamalionDev@users.noreply.github.com>
2025-04-01 14:24:03 -0600

    commit 3 1Abril2025

--- lib/ui/views/login_view.dart
+++ lib/ui/views/login_view.dart
@@ -1,3 +1,4 @@
+import 'package:e301_web/ui/buttons/links_text.dart';
 import 'package:flutter/material.dart';
 
 class LoginView extends StatelessWidget {
@@ -6,10 +7,59 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.red,
+      margin: EdgeInsets.only(top: 100),
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      color: Colors.black,
       child: Center(
-        child: Text('Login View'),
+        child: ConstrainedBox(
+          constraints: BoxConstraints(maxWidth: 370),
+          child: Form(
+            child: Column(
+              children: [
+                TextFormField(
+                  style: TextStyle(color: Colors.white),
+                  decoration: buildInputDecoration(
+                    hint: 'email',
+                    label: 'Correo Electrónico',
+                    icon: Icons.email_outlined,
+                  ),
+                ),
+                SizedBox(height: 10,),
+                TextFormField(
+                  style: TextStyle(color: Colors.white),
+                  decoration: buildInputDecoration(
+                    hint: '******',
+                    label: 'Contraseña',
+                    icon: Icons.lock_clock_outlined,
+                  ),
+                ),
+                SizedBox(height: 20),
+                LinksText(text: 'Nueva Cuenta'),
+              ],
+            ),
+          ),
+        ),
       ),
+    );
+  }
+
+  InputDecoration buildInputDecoration({
+    required String hint,
+    required String label,
+    required IconData icon,
+  }){
+    return InputDecoration(
+      border: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.white.withAlpha(100)),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.white.withAlpha(100)),
+      ),
+      hintText: hint,
+      labelText: label,
+      prefixIcon: Icon(icon,color: Colors.grey.withAlpha(150)),
+      labelStyle: TextStyle(color: Colors.grey),
+      hintStyle: TextStyle(color: Colors.grey),
     );
   }
 }
