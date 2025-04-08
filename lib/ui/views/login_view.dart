24be757bb32e799a71d1bc4572b7321c44e1e16d
XamalionDev <132012434+XamalionDev@users.noreply.github.com>
2025-04-08 14:21:44 -0600

    commit 1 8Abr2025

--- lib/ui/views/login_view.dart
+++ lib/ui/views/login_view.dart
@@ -1,58 +1,87 @@
 import 'package:e301_web/config/router/router.dart';
+import 'package:e301_web/providers/login_form_provider.dart';
 import 'package:e301_web/ui/buttons/custom_outlined_button.dart';
 import 'package:e301_web/ui/buttons/links_text.dart';
 import 'package:e301_web/ui/inputs/custom_inputs.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: 100),
-      padding: EdgeInsets.symmetric(horizontal: 20),
-      color: Colors.black,
-      child: Center(
-        child: ConstrainedBox(
-          constraints: BoxConstraints(maxWidth: 370),
-          child: Form(
-            child: Column(
-              children: [
-                TextFormField(
-                  style: TextStyle(color: Colors.white),
-                  decoration: CustomInputs.loginInputDecoration(
-                    hint: 'Ingrese su correo',
-                    label: 'Correo Electrónico',
-                    icon: Icons.email_outlined,
+    return ChangeNotifierProvider(
+      create: (context) => LoginFormProvider(),
+      child: Builder(
+        builder: (context) {
+          final loginFormProvider = Provider.of<LoginFormProvider>(
+            listen: false,
+            context,
+          );
+
+          return Container(
+            margin: EdgeInsets.only(top: 100),
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            color: Colors.black,
+            child: Center(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(maxWidth: 370),
+                child: Form(
+                  key: loginFormProvider.formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        style: TextStyle(color: Colors.white),
+                        decoration: CustomInputs.loginInputDecoration(
+                          hint: 'Ingrese su correo',
+                          label: 'Correo Electrónico',
+                          icon: Icons.email_outlined,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      TextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Ingrese su contraseña';
+                          }
+                          if (value.length < 6) {
+                            return 'La contraseña debe tener al menos 6 caracteres';
+                          }
+                          return null;
+                        },
+                        style: TextStyle(color: Colors.white),
+                        decoration: CustomInputs.loginInputDecoration(
+                          hint: '******',
+                          label: 'Contraseña',
+                          icon: Icons.lock_clock_outlined,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      CustomOutlinedButton(
+                        onPressed: () {
+                          loginFormProvider.validateForm();
+                        },
+                        text: 'Ingresar',
+                        isFilled: true,
+                      ),
+                      SizedBox(height: 20),
+                      LinksText(
+                        text: 'Nueva Cuenta',
+                        onPressed: () {
+                          Navigator.pushNamed(
+                            context,
+                            Flurorouter.registerRoute,
+                          );
+                        },
+                      ),
+                    ],
                   ),
                 ),
-                SizedBox(height: 10,),
-                TextFormField(
-                  style: TextStyle(color: Colors.white),
-                  decoration: CustomInputs.loginInputDecoration(
-                    hint: '******',
-                    label: 'Contraseña',
-                    icon: Icons.lock_clock_outlined,
-                  ),
-                ),
-                SizedBox(height: 20),
-                CustomOutlinedButton(
-                  onPressed: (){},
-                  text: 'Ingresar',
-                  isFilled: true,
-                ),
-                SizedBox(height: 20),
-                LinksText(
-                  text: 'Nueva Cuenta',
-                  onPressed: (){
-                    Navigator.pushNamed(context, Flurorouter.registerRoute);
-                  },
-                ),
-              ],
+              ),
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
--- lib/ui/views/register_view.dart
+++ lib/ui/views/register_view.dart
@@ -9,56 +9,58 @@ class RegisterView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: 100),
-      padding: EdgeInsets.symmetric(horizontal: 20),
-      color: Colors.black,
-      child: Center(
-        child: ConstrainedBox(
-          constraints: BoxConstraints(maxWidth: 370),
-          child: Form(
-            child: Column(
-              children: [
-                TextFormField(
-                  style: TextStyle(color: Colors.white),
-                  decoration: CustomInputs.loginInputDecoration(
-                    hint: 'Ingrese su nombre',
-                    label: 'Nombre',
-                    icon: Icons.supervised_user_circle_outlined,
+    return SingleChildScrollView(
+      child: Container(
+        margin: EdgeInsets.only(top: 100),
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        color: Colors.black,
+        child: Center(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(maxWidth: 370),
+            child: Form(
+              child: Column(
+                children: [
+                  TextFormField(
+                    style: TextStyle(color: Colors.white),
+                    decoration: CustomInputs.loginInputDecoration(
+                      hint: 'Ingrese su nombre',
+                      label: 'Nombre',
+                      icon: Icons.supervised_user_circle_outlined,
+                    ),
                   ),
-                ),
-                SizedBox(height: 10,),
-                TextFormField(
-                  style: TextStyle(color: Colors.white),
-                  decoration: CustomInputs.loginInputDecoration(
-                    hint: 'email',
-                    label: 'Correo Electrónico',
-                    icon: Icons.email_outlined,
+                  SizedBox(height: 10,),
+                  TextFormField(
+                    style: TextStyle(color: Colors.white),
+                    decoration: CustomInputs.loginInputDecoration(
+                      hint: 'email',
+                      label: 'Correo Electrónico',
+                      icon: Icons.email_outlined,
+                    ),
                   ),
-                ),
-                SizedBox(height: 10,),
-                TextFormField(
-                  style: TextStyle(color: Colors.white),
-                  decoration: CustomInputs.loginInputDecoration(
-                    hint: '******',
-                    label: 'Contraseña',
-                    icon: Icons.lock_clock_outlined,
+                  SizedBox(height: 10,),
+                  TextFormField(
+                    style: TextStyle(color: Colors.white),
+                    decoration: CustomInputs.loginInputDecoration(
+                      hint: '******',
+                      label: 'Contraseña',
+                      icon: Icons.lock_clock_outlined,
+                    ),
                   ),
-                ),
-                SizedBox(height: 20),
-                CustomOutlinedButton(
-                  onPressed: (){},
-                  text: 'Crear Cuenta',
-                  isFilled: true,
-                ),
-                SizedBox(height: 20),
-                LinksText(
-                  text: 'Ir al login',
-                  onPressed: (){
-                    Navigator.pushNamed(context, Flurorouter.loginRoute);
-                  },
-                ),
-              ],
+                  SizedBox(height: 20),
+                  CustomOutlinedButton(
+                    onPressed: (){},
+                    text: 'Crear Cuenta',
+                    isFilled: true,
+                  ),
+                  SizedBox(height: 20),
+                  LinksText(
+                    text: 'Ir al login',
+                    onPressed: (){
+                      Navigator.pushNamed(context, Flurorouter.loginRoute);
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
         ),
