c1e8f92fbb51f20ec890b853050d507431549f1c
XamalionDev <132012434+XamalionDev@users.noreply.github.com>
2025-05-12 09:58:17 -0600

    commit3 12May2025

--- lib/ui/shared/sidebar.dart
+++ lib/ui/shared/sidebar.dart
@@ -8,7 +8,28 @@ class Sidebar extends StatelessWidget {
     return Container(
       width: 200,
       height: double.infinity,
-      color: Colors.amber,
+      decoration: buildBoxDecoration(),
+      child: ListView(
+        physics: ClampingScrollPhysics(),
+        children: [
+
+        ],
+      ),
     );
   }
+
+  BoxDecoration buildBoxDecoration() => BoxDecoration(
+    gradient: LinearGradient(
+      colors: [
+        Color(0xff092044),
+        Color(0xff092042),
+      ]
+    ),
+    boxShadow: [
+      BoxShadow(
+        color: Colors.black38,
+        blurRadius: 10,
+      )
+    ]
+  );
 }
