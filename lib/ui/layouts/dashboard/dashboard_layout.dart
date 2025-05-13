518914335f68b712234960bdf3f8bc968ed6eaec
XamalionDev <132012434+XamalionDev@users.noreply.github.com>
2025-05-13 13:56:31 -0600

    commit5 13May2025

--- lib/ui/layouts/dashboard/dashboard_layout.dart
+++ lib/ui/layouts/dashboard/dashboard_layout.dart
@@ -5,34 +5,37 @@ import 'package:flutter/material.dart';
 class DashboardLayout extends StatelessWidget {
   final Widget child;
 
-  const DashboardLayout({
-    super.key,
-    required this.child
-  });
+  const DashboardLayout({super.key, required this.child});
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       backgroundColor: Colors.grey.shade100,
-      body: Row(
+      body: Stack(
         children: [
-            //TODO: Si es mayor a 700px
-            Sidebar(),
-            
-            Expanded(
-              child: Column(
-                children: [
-                  // Barra de navegación
-                  Navbar(),
-              
-                  Expanded(
-                    child: child
-                  ),
-                ],
+          Row(
+            children: [
+              //TODO: Si es mayor a 700px
+              if (size.width >= 700) Sidebar(),
+
+              Expanded(
+                child: Column(
+                  children: [
+                    // Barra de navegación
+                    Navbar(),
+
+                    Expanded(child: child),
+                  ],
+                ),
               ),
-            ),
-          ],
-      )
+            ],
+          ),
+          if(size.width<700)
+            Sidebar(),
+        ],
+      ),
     );
   }
 }
