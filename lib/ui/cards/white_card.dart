a230dfcab01ec5e566a0b95b15146e02829db370
XamalionDev <132012434+XamalionDev@users.noreply.github.com>
2025-05-19 10:33:36 -0600

    commit5 19May2025

--- lib/ui/cards/white_card.dart
+++ lib/ui/cards/white_card.dart
@@ -1,12 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WhiteCard extends StatelessWidget {
-  const WhiteCard({super.key});
+  final String? title;
+  final Widget child;
+
+  const WhiteCard({
+    super.key,
+    this.title,
+    required this.child
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      margin: EdgeInsets.all(8),
+      padding: EdgeInsets.all(10),
+      decoration: buildBoxDecoration(),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if(title!=null)
+          ...[
+            FittedBox(
+              fit: BoxFit.contain,
+              child: Text(
+                title!,
+                style: GoogleFonts.roboto(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+            Divider(),
+          ]
+        ],
+      ),
     );
   }
+
+  BoxDecoration buildBoxDecoration() => BoxDecoration(
+    color: Colors.white,
+    borderRadius: BorderRadius.circular(5),
+    boxShadow: [
+      BoxShadow(
+        color: Colors.black.withAlpha(10),
+        blurRadius: 5
+      )
+    ]
+  );
 }
