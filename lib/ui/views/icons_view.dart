162a61bf0683dbfc90fdd3277f57043c4f66f761
XamalionDev <132012434+XamalionDev@users.noreply.github.com>
2025-05-20 13:26:06 -0600

    commit2 20May2025

--- lib/ui/views/icons_view.dart
+++ lib/ui/views/icons_view.dart
@@ -13,10 +13,38 @@ class IconsView extends StatelessWidget {
           Text('Icons View',style: CustomLabels.h1),
           SizedBox(height: 10),
           Wrap(
+            crossAxisAlignment: WrapCrossAlignment.start,
+            direction: Axis.horizontal,
             children: [
               WhiteCard(
                 title: 'ac_unit_outlined',
-                child: Icon(Icons.ac_unit_outlined)
+                width: 170,
+                child: Center(child: Icon(Icons.ac_unit_outlined)),
+              ),
+              WhiteCard(
+                title: 'apps_outage',
+                width: 170,
+                child: Center(child: Icon(Icons.apps_outage)),
+              ),
+              WhiteCard(
+                title: 'adobe',
+                width: 170,
+                child: Center(child: Icon(Icons.adobe)),
+              ),
+              WhiteCard(
+                title: 'beenhere',
+                width: 170,
+                child: Center(child: Icon(Icons.beenhere)),
+              ),
+              WhiteCard(
+                title: 'network_wifi',
+                width: 170,
+                child: Center(child: Icon(Icons.network_wifi)),
+              ),
+              WhiteCard(
+                title: 'system_update',
+                width: 170,
+                child: Center(child: Icon(Icons.system_update)),
               ),
             ],
           )
